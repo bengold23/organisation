0c5fd9391ecc47f3de4a1d20356047c50657ff77
ilyes hachem <45404067+bengold23@users.noreply.github.com>
2019-06-15 03:49:43 +0200

    Add files via upload

---
+++ es_extended/config.lua
@@ -0,0 +1,12 @@
+Config                          = {}
+Config.MaxPlayers               = 255
+Config.Accounts                 = { 'bank', 'black_money' }
+Config.AccountLabels            = { bank = 'Banque', black_money = 'Argent Sale' } -- French
+-- Config.AccountLabels            = { bank = 'Bank', black_money = 'Dirty Money' } -- English
+Config.PaycheckInterval         = 30 * 60000
+Config.ShowDotAbovePlayer       = false
+Config.DisableWantedLevel       = true
+Config.RemoveInventoryItemDelay = 1 * 60000
+Config.EnableWeaponPickup       = true
+Config.Locale                   = 'fr'
+Config.EnableDebug              = false
